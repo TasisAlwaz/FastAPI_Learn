ed95e291b14798f7ba1cd6606afdfdbf1c70bb70
TasisAlwaz <yttasis.alwaz@gmail.com>
2026-09-21 18:22:39 +0530

    Chapter 9: SQLAlchemy model creation, Database connection establish pgAdmin4

--- database.py
+++ database.py
@@ -2,6 +2,6 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
 
-db_url = "postgresql://postgres:12345678@localhost:5432/telusko"
+db_url = "postgresql://postgres:postgres@localhost:5432/Telusko"
 engine = create_engine(db_url )
 session = sessionmaker(autocommit = False, autoflush= False, bind = engine)
