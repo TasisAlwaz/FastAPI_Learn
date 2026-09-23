bfe621af92b2130d4bae0522fb4e3888502db6e5
TasisAlwaz <yttasis.alwaz@gmail.com>
2026-09-23 13:03:18 +0530

    Chapter 10: Initializing Data

--- database.py
+++ database.py
@@ -4,4 +4,8 @@ from sqlalchemy import create_engine
 
 db_url = "postgresql://postgres:postgres@localhost:5432/Telusko"
 engine = create_engine(db_url )
-session = sessionmaker(autocommit = False, autoflush= False, bind = engine)
+session = sessionmaker(
+    autocommit = False,
+    autoflush= False, 
+    bind = engine
+    )
